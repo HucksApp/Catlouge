7a2cda038e9c50a680e0d32e1062e6dea06d2246
hucksapp@gmail.com <hucksapp@gmail.com>
2021-06-18 03:34:55 +0100

    server and UI

---
+++ server/src/db/models/type.ts
@@ -0,0 +1,5 @@
+export type book = { name:string, genre:string, id:string, authorId:string }
+export type author = { name:string, age:Number, id:string }
+export type handler = (parent:any, args:any ) => Object
+
+
